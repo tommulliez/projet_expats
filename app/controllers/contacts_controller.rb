33ce2b26983c1d13d7a8f04241540a1975937c74
tommulliez <mulliez.thomas@gmail.com>
2017-09-28 09:01:28 +0200

    fake message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,15 +4,18 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash[:notice] = "Votre message a bien été envoyé"
-      redirect_to root_path
-    else
-      flash[:alert] = "Votre message n'a pas été transmis, merci d'essayer à nouveau"
-      redirect_to root_path
-    end
+    flash[:notice] = "Votre message a bien été envoyé"
+    redirect_to root_path
+    # a repositionner une fois le nom de domaine et adresse email reçus
+    # @contact = Contact.new(params[:contact])
+    # @contact.request = request
+    # if @contact.deliver
+    #   flash[:notice] = "Votre message a bien été envoyé"
+    #   redirect_to root_path
+    # else
+    #   flash[:alert] = "Votre message n'a pas été transmis, merci d'essayer à nouveau"
+    #   redirect_to root_path
+    # end
   end
 
 end
